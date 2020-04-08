3d0841ebe1586c9e8182036c63c80855c1198228
DigitEngine <39778493+DigitEngine@users.noreply.github.com>
2020-04-08 13:02:12 -0400

    Create images.js

---
+++ js/images.js
@@ -0,0 +1,63 @@
+var monasteryTiles = [
+  ".images/tiles/monastery/transparent.png",
+  ".images/tiles/monastery/crossTile00.png",
+  ".images/tiles/monastery/crossTile01.png",
+  ".images/tiles/monastery/crossTile02.png",
+  ".images/tiles/monastery/crossTile03.png",
+  ".images/tiles/monastery/crossTile04.png",
+  ".images/tiles/monastery/crossTile05.png",
+  ".images/tiles/monastery/crossBase00.png",
+  ".images/tiles/monastery/crossBase01.png",
+  ".images/tiles/monastery/crossBase02.png",
+  ".images/tiles/monastery/spike00.png",
+  ".images/tiles/monastery/spike01.png",
+  ".images/tiles/monastery/solid_OCornerTopL.png",
+  ".images/tiles/monastery/solid_OCornerTopR.png",
+  ".images/tiles/monastery/solid_OCornerBotL.png",
+  ".images/tiles/monastery/solid_OCornerBotR.png",
+  ".images/tiles/monastery/solid_FlatT.png",
+  ".images/tiles/monastery/solid_FlatB.png",
+  ".images/tiles/monastery/solid_FlatL.png",
+  ".images/tiles/monastery/solid_FlatR.png",
+  ".images/tiles/monastery/solid_ICornerTopL.png",
+  ".images/tiles/monastery/solid_ICornerTopR.png",
+  ".images/tiles/monastery/solid_ICornerBotL.png",
+  ".images/tiles/monastery/solid_ICornerBotR.png",
+  ".images/tiles/monastery/solid_ThinVertM.png",
+  ".images/tiles/monastery/solid_ThinHorizM.png",
+  ".images/tiles/monastery/solid_ThinVertT.png",
+  ".images/tiles/monastery/solid_ThinVertB.png",
+  ".images/tiles/monastery/solid_ThinHorizL.png",
+  ".images/tiles/monastery/solid_ThinHorizR.png"
+  ".images/tiles/monastery/solid_Middle.png",
+  ".images/tiles/monastery/brick_Brown.png",
+  ".images/tiles/monastery/brick_PurpleInt.png".
+  ".images/tiles/monastery/brick_PurpleTopL.png",
+  ".images/tiles/monastery/brick_PurpleTopM.png",
+  ".images/tiles/monastery/brick_PurpleTopR.png",
+  ".images/tiles/monastery/brick_PurpleR.png",
+  ".images/tiles/monastery/brick_PurpleBotL.png",
+  ".images/tiles/monastery/brick_PurpleBotM.png",
+  ".images/tiles/monastery/brick_PurpleBotR.png",
+  ".images/tiles/monastery/brick_PurpleL.png",
+  ".images/tiles/monastery/sky_NoStars.png",
+  ".images/tiles/monastery/sky_Stars00.png",
+  ".images/tiles/monastery/sky_Stars01.png",
+  ".images/tiles/monastery/sky_Stars02.png",
+  ".images/tiles/monastery/sky_Stars03.png",
+  ".images/tiles/monastery/pillar_TopL.png",
+  ".images/tiles/monastery/pillar_TopR.png",
+  ".images/tiles/monastery/pillar_MidL.png",
+  ".images/tiles/monastery/pillar_MidR.png",
+  ".images/tiles/monastery/pillar_BotL.png",
+  ".images/tiles/monastery/pillar_BotR.png",
+];
+var forestTiles = [ ];
+var desertTiles = [ ];
+var egyptTiles = [ ];
+var rainforestTiles = [ ];
+var templeTiles = [ ];
+var clocktowerTiles = [ ];
+var jupiterTiles = [ ];
+var saturnTiles = [ ];
+var chinaTiles = [ ];
