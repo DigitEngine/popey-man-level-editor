172cc4d9f9264e88561a5e3a1aef88bd67065ed9
DigitEngine <39778493+DigitEngine@users.noreply.github.com>
2020-04-08 13:27:45 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var cvs = document.getElementById("cvs");
+var ctx = cvs.getContext("2d");
+
+var screenSizeX = 640;
+var screenSizeY = 480;
+
+var tileSize = 16;
+
+var tilesX = screenSizeX / tileSize;
+var tilesY = screenSizeY / tileSize;
